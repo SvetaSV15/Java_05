590aa702bb70198ec8b35db025206dffd8c7ebc5
SvetaSV15 <temnaya.87@mail.ru>
2022-10-19 23:45:40 +0500

    FizzBuzz algo and Tests added

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,85 @@
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class FizzBuzzTest {
+
+    //1.Positive testing Happy path
+    // if(start <= end)
+    //return array;
+
+    //start < end
+    @Test
+    public void testStartLessThanEnd_HappyPath(){
+    //AAA
+        //Arrange
+        int start = 1;
+        int end = 20;
+        String[] expectedResualt = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11",
+                "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+        //act
+
+        String [] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResualt);
+    }
+
+
+    //start == end
+    @Test
+    public void testStartEqualsThanEnd_HappyPath(){
+        //AAA
+        //Arrange
+        int start = 1;
+        int end = 1;
+        String[] expectedResualt = {"1"};
+        //act
+
+        String [] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResualt);
+    }
+
+
+//start < end
+
+    @Test
+    public void testStartLessThanEnd_StartEndAreNegative_HappyPath(){
+        //AAA
+        //Arrange
+        int start = -20;
+        int end = -1;
+        String[] expectedResualt = {"Buzz", "-19", "Fizz", "-17", "-16", "FizzBuzz", "-14", "-13", "Fizz",
+                "-11", "Buzz", "Fizz", "-8", "-7", "Fizz", "Buzz", "-4", "Fizz", "-2", "-1"};
+        //act
+
+        String [] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResualt);
+    }
+
+    //1.Negative testing
+    // if(start > end)
+    //return new String[0];
+
+    @Test
+    public void testStartGreaterThanEnd_Negative(){
+        //AAA
+        //Arrange
+        int start = 20;
+        int end = 1;
+        String[] expectedResualt = {};
+        //act
+
+        String [] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResualt);
+    }
+
+
+
+}
